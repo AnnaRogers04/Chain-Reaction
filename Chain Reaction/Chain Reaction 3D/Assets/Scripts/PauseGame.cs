b607d9db32057df596ee1f0ed558bc1e9e5d4ba0
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2025-01-30 23:39:18 +0000

    .

---
+++ Chain Reaction/Chain Reaction 3D/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject PauseScreen;
+    private bool isShowing;
+    public bool lockcursor = true;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isShowing = !isShowing;
+            PauseScreen.SetActive(isShowing);
+            lockcursor = !lockcursor;
+        }
+        Cursor.lockState = lockcursor?CursorLockMode.Locked:CursorLockMode.None;
+        Cursor.visible = !lockcursor;
+    }
+}
--- Chain Reaction/Chain Reaction 3D/Assets/Scripts/start and end/MainMenu.cs
+++ Chain Reaction/Chain Reaction 3D/Assets/Scripts/start and end/MainMenu.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadGame()
+    public void LoadMenu()
     {
         SceneManager.LoadScene(0);
     }
