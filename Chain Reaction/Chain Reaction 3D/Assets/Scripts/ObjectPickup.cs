04008300f7b7cdf4ebc28d34f07df135752a753d
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2025-01-30 02:56:38 +0000

    gem isnt registering

--- Chain Reaction/Chain Reaction 3D/Assets/Scripts/ObjectPickup.cs
+++ Chain Reaction/Chain Reaction 3D/Assets/Scripts/ObjectPickup.cs
@@ -5,13 +5,19 @@ public class ObjectPickup : MonoBehaviour
 //    LayerMask _layerMask = LayerMask.GetMask("PickUp");
     private void Update()
     {
-        if (Input.GetMouseButtonDown('E'))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
             { 
                 Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow); 
-                Debug.Log("Did Hit"); 
+                Debug.Log("Did Hit");
+               
+                if (hit.collider.CompareTag("Gems"))
+                {
+                    Destroy(hit.collider.gameObject); 
+                    Debug.Log("Gems Hit");
+                }
             }
             else
             { 
