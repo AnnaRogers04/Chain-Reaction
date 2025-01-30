25b08c4e9d4914ba8676bea30b899d2b49fd8a89
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2025-01-30 01:52:31 +0000

    .

--- Chain Reaction/Chain Reaction 3D/Assets/Scripts/ObjectPickup.cs
+++ Chain Reaction/Chain Reaction 3D/Assets/Scripts/ObjectPickup.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class ObjectPickup : MonoBehaviour
 {
-    LayerMask _layerMask = LayerMask.GetMask("PickUp");
+//    LayerMask _layerMask = LayerMask.GetMask("PickUp");
     private void Update()
     {
         if (Input.GetMouseButtonDown('E'))
         {
             RaycastHit hit;
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, _layerMask))
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
             { 
                 Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow); 
                 Debug.Log("Did Hit"); 
